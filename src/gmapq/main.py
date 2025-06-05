1e915ee722bbaa76a4fe0c27eccc496f31478d6b
Lesly Kinzel <lesly.helios@proton.me>
2025-06-05 15:56:05 +1000

    Remove dotenv dependency

--- src/gmapq/main.py
+++ src/gmapq/main.py
@@ -1,9 +1,18 @@
 # gmapq
+
+# std
+import os
 import sys
 
 
 def main():
     return 0
+
+
+def getenv(key: str) -> str|None:
+    return os.getenv(key) or (print(
+        f"Missing environment variable: {key}"
+    ), sys.exit(1))[0]
 
 
 if __name__ == "__main__":
